fd3d5cf53fe4f27c85ad199d37da92a5aed0ef57
l9kk <askhat.ss23@gmail.com>
2024-11-28 02:22:19 +0500

    "+"

--- myforum/settings.py
+++ myforum/settings.py
@@ -138,10 +138,15 @@ WSGI_APPLICATION = 'myforum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sdu_surv_kit',
+        'USER': 'sigma',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_USER_MODEL = "api.User"
 
